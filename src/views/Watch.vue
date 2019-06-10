9a40d99ed63eb6bb7fc9695d86f6f924cc558134
saein.lee <gitSaein@gmail.com>
2019-06-10 21:36:00 +0900

    watch v-model

---
+++ src/views/Watch.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="watch">
+      <p>{{message}}</p>
+      <input type="text" v-model="watchedTest">
+      {{watchedTest}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'watch',
+    data: function(){
+        return {
+          message: 'before',
+          watchedTest: 'initialize'
+        }
+    },
+    watch:{
+        'watchedTest': 'alertWatched'
+    },
+    methods: {
+        alertWatched(){
+            alert(this.watchedTest)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
